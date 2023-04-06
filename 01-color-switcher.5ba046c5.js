45ef546cfa5dd26af9126b4976931ae47502b392
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-04-06 10:08:12 +0000

    Deploying to gh-pages from @ 1Kostik/goit-js-hw-09@d7bae5035cde14e42e9a8c63afafe804fe554274 🚀

---
+++ 01-color-switcher.5ba046c5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.body,a=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.setAttribute("disabled",!0),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.5ba046c5.js.map
