93d1e180627550631708190b8a20c718010e8818
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-04-10 01:05:13 +0300

    ц

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,81 +1,63 @@
+// Описаний в документації
+'use strict';
 import flatpickr from 'flatpickr';
-
+// Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-Notify.init({
-  position: 'center-top',
-  cssAnimationStyle: 'zoom',
-  fontSize: '18px',
-  width: '350px',
-});
-
+import { Notify } from 'notiflix';
+let timerId = null;
 const refs = {
-  inputDate: document.querySelector('#datetime-picker'),
-  btnStart: document.querySelector('[data-start]'),
-  body: document.querySelector('body'),
+  getElInputDate: document.getElementById('datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMinutes: document.querySelector('[data-minutes]'),
+  dataSeconds: document.querySelector('[data-seconds]'),
 };
-
-refs.btnStart.disabled = true;
+refs.startBtn.setAttribute('disabled', true);
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {},
-  onClose: function (selectedDates, dateStr, instance) {
-    if (selectedDates[0] <= new Date()) {
-      Notify.failure('Please choose a date in the future');
-      refs.btnStart.disabled = true;
+  onClose(selectedDates) {
+    const currentTime = new Date();
+    if (selectedDates[0] - currentTime > 0) {
+      refs.startBtn.removeAttribute('disabled');
     } else {
-      refs.btnStart.disabled = false;
+      refs.startBtn.disabled = true;
+      Notify.failure('Please choose a date in the future');
     }
   },
 };
-
 const fp = flatpickr('#datetime-picker', options);
 
-refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.startBtn.addEventListener('click', onClickStartBtn);
 
-function onBtnStartClick() {
-  const futureDate = fp.selectedDates[0].getTime();
-  refs.inputDate.disabled = true;
-  refs.btnStart.disabled = true;
-
-  const intervalId = setInterval(() => {
-    const currentDate = Date.now();
-    const ms = futureDate - currentDate;
+function onClickStartBtn() {
+  start();
+}
+function start() {
+  timerId = setInterval(() => {
+    const selectDate = fp.selectedDates[0].getTime();
+    const dataNew = new Date().getTime();
+    const ms = selectDate - dataNew;
     if (ms < 0) {
-      refs.inputDate.disabled = false;
-      refs.btnStart.disabled = false;
-      clearInterval(intervalId);
-      refs.body.insertAdjacentHTML(
-        'beforeend',
-        '<iframe src="https://gifer.com/embed/2DV" width=480 height=216.960 frameBorder="0" allowFullScreen></iframe>'
-      );
+      clearInterval(timerId);
       return;
     }
-    updateTimer(convertMs(ms));
+
+   updateTimer(convertMs(ms));
   }, 1000);
 }
-
-function pad(value) {
-  return String(value).padStart('2', 0);
-}
-
-const timer = {
-  days: document.querySelector('[data-days]'),
-  hours: document.querySelector('[data-hours]'),
-  minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]'),
-};
-
 function updateTimer({ days, hours, minutes, seconds }) {
-  timer.days.textContent = `${days}`;
-  timer.hours.textContent = `${hours}`;
-  timer.minutes.textContent = `${minutes}`;
-  timer.seconds.textContent = `${seconds}`;
+  refs.dataDays.textContent = `${days}`;
+  refs.dataHours.textContent = `${hours}`;
+  refs.dataMinutes.textContent = `${minutes}`;
+  refs.dataSeconds.textContent = `${seconds}`;
+}
+function pad(value) {
+  return String(value).padStart(2, '0');
 }
 
 function convertMs(ms) {
